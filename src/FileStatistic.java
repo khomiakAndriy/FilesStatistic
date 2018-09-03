01616b5a620343dd00f9e821881fd8460ee1ced3
khomiakAndriy <khomiak.andriy@gmail.com>
2018-09-03 17:22:07 +0300

    small refactoring with thread-safe 2

--- src/FileStatistic.java
+++ src/FileStatistic.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 
 public class FileStatistic {
@@ -12,18 +10,19 @@ public class FileStatistic {
         File folder = new File("F:/tmp/");
         List<File> files = new ArrayList<>();
         StatisticUtils.listFilesFromFolder(files, folder);
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+//        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        List<Future<Map<String, Integer>>> furures = addFilesToExecutor(files, executor);
+        List<Map.Entry<String, Integer>> listOfEntriesFromParralelStreams = files.parallelStream().map(file -> StatisticUtils.getWordsStatisticFromFile(file)).flatMap(map -> map.entrySet().stream()).collect(Collectors.toList());
+        Map<String, Integer> finalStatistic = getFinalStatisticUsingParallelResult(listOfEntriesFromParralelStreams);
 
-        Map<String, Integer> finalStatistic = getFinalStatistic(furures);
-
+        System.out.println("Quantity of unique word " + finalStatistic.size());
+        System.out.println("All words count " + finalStatistic.values().stream().mapToInt(i -> i).sum());
         System.out.println("Files statistic:");
         System.out.println("Word - count");
 
         finalStatistic.forEach((key, value) -> System.out.println(key + " - " + value));
 
-        executor.shutdown();
+//        executor.shutdown();
     }
 
     private static List<Future<Map<String, Integer>>> addFilesToExecutor(List<File> files, ExecutorService executor) {
@@ -48,6 +47,21 @@ public class FileStatistic {
                 } else {
                     finalStatistic.put(key, finalStatistic.get(key)+value);
                 }
+            }
+        }
+        return finalStatistic;
+    }
+
+    private static  Map<String, Integer> getFinalStatisticUsingParallelResult(List<Map.Entry<String, Integer>> mapEntries) throws InterruptedException, ExecutionException {
+        Map<String, Integer> finalStatistic = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : mapEntries){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if (!finalStatistic.containsKey(key)) {
+                finalStatistic.put(key, value);
+            } else {
+                finalStatistic.put(key, finalStatistic.get(key) + value);
             }
         }
         return finalStatistic;
